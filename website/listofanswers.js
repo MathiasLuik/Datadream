9ee754cf9f07b231aa7e903b8de4094a8a596af4
Mathias Luik <mathias.luik@gmail.com>
2018-09-24 17:48:52 +0300

    up

---
+++ website/listofanswers.js
@@ -0,0 +1,15 @@
+
+var data = [   
+    ["US","Beverages, spirits and vinegar","singapore"],
+    ["China","Coffee, tea and spices","switzerland"],
+    ["Italy","Fruit and nuts","israel"],
+    ["US","Furniture","denmark"],
+    ["US","Iron and steel","hong kong"],
+    ["US","Musical instruments","hong kong"],
+    ["US","Pharmaceutical products","denmark"],
+    ["US","Aircrafts","hong kong"],
+    ["US","Toys and games","denmark"],
+    ["US","Wood and articles of wood","hong kong"],
+    ["US","Art, collectors pieces and antiques","denmark"],
+    ["US","Yachts,rowing boats and canoes","denmark"],
+]
